1934b8f390553a4606004f6d3299c1b3834f9516
kdk1248 <alice3976@g.hongik.ac.kr>
2025-02-10 12:50:48 +0900

    feat: 게시글 조회 Fetch API 요청문 작성과 데이터 바인딩 구현

--- src/app/articles/articles-list/articles-list.page.ts
+++ src/app/articles/articles-list/articles-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/common/article-response.interface';
+import { ArticlesService } from '../articles.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-articles-list',
@@ -7,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   standalone:false
 })
 export class ArticlesListPage implements OnInit {
+  articles: Article[] = [];
 
-  constructor() { }
+  constructor(
+    private articlesService: ArticlesService,
+    private router: Router //경로 이동을 위함(앵귤러에서 지원원)
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      const response = await this.articlesService.getAllArticles();
+      if(response.success){
+        this.articles = response.data;
+      }else{
+        console.error(response.message);
+      }
+    } catch (error) {
+      console.error('Fetch error', error);
+      
+    }
   }
 
+  //route Article detail page
+  viewArticle(id: number){
+    this.router.navigate([`${id}/detail`])
+
+  }
 }
